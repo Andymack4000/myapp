24eccccae9bfdcf84b84f2def21673c149ae3e86
Andymack4000 <andymack4000@gmail.com>
2024-07-25 06:23:34 +0100

    Initial Author Routes

--- server.js
+++ server.js
@@ -7,12 +7,15 @@ const app = express()
 const expressLayouts = require('express-ejs-layouts')
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 const mongoose = require('mongoose')
 
@@ -22,5 +25,6 @@ db.on('error', error => console.log(error))
 db.once('open', () => console.log('Connected to mongoose'))
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || 3000)
