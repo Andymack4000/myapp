bd6cfe8cb55ada4f5afd65f9ccfe09cd62a21588
Andymack4000 <andymack4000@gmail.com>
2024-08-20 06:49:40 +0100

    Finish book Routes

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const methodOverride = require('method-override')
 
 const indexRouter = require('./routes/index')
 const authorRouter = require('./routes/authors')
@@ -14,9 +15,10 @@ app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
+app.use(methodOverride('_method'))
 app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+app.use(express.urlencoded({ limit: 52428800, extended: true }))
+app.use(express.json({limit : 52428800}))
 
 const mongoose = require('mongoose')
 
